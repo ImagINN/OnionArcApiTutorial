2676da3d2cca8282bdbdb661798832e7de8ae54a
Gökhan SAL <muhammed.gokhan55@gmail.com>
2025-04-16 23:09:40 +0300

    project rules tamamlandı

---
+++ Core/OnionArc.Application/Features/Products/Rules/ProductRules.cs
@@ -0,0 +1,20 @@
+﻿using OnionArc.Application.Bases;
+using OnionArc.Application.Features.Products.Exceptions;
+using OnionArc.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionArc.Application.Features.Products.Rules
+{
+    public class ProductRules : BaseRules
+    {
+        public Task ProductTitleMustNotBeSame(IList<Product> products, string requestTitle)
+        {
+            if (products.Any(x => x.Title == requestTitle)) throw new ProductTitleMustNotBeSameException();
+            return Task.CompletedTask;
+        }
+    }
+}
