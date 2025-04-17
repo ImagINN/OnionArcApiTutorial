6fe3de1b0c5f93f96d9f6f8276ddcc404fa1fb80
Gökhan SAL <muhammed.gokhan55@gmail.com>
2025-04-17 08:55:44 +0300

    Token servisi tamamlandı. Swagger'da auth arayüzü oluşturuldu.

--- Infrastructure/OnionArc.Infrastructure/Registration.cs
+++ Infrastructure/OnionArc.Infrastructure/Registration.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using OnionArc.Application.Interfaces.Tokens;
 using OnionArc.Infrastructure.Tokens;
 using System;
 using System.Collections.Generic;
@@ -16,5 +19,26 @@ public static class Registration
     public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         services.Configure<TokenSettings>(configuration.GetSection("JWT"));
+        services.AddTransient<ITokenService, TokenService>();
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+        {
+            options.SaveToken = true;
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"])),
+                ValidateLifetime = false,
+                ValidIssuer = configuration["JWT:Issuer"],
+                ValidAudience = configuration["JWT:Audience"],
+                ClockSkew = TimeSpan.Zero
+            };
+        });
     }
 }
